d505a47ff9bca86f2d341b2c913a66535ed32093
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-06-09 22:27:34 -0400

    feat: route login and home pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { authGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
   {
+    path: '',
+    loadChildren: () => import('./_pages/login/login.module').then(m => m.LoginPageModule)
+  },
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [authGuard],
+    children: [
+      {
+        path: 'home',
+        loadChildren: () => import('./_pages/home/home.module').then(m => m.HomePageModule)
+      }
+    ]
   }
 ];
 @NgModule({
