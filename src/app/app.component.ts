21acadaf5ab7c0bfaaaeabad6f0e8fc143dc1ac5
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-06-09 22:26:27 -0400

    feat: set current auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './_services/auth.service';
+import { Auth } from './_interfaces/auth';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+    this.setCurrentAuth();
+  }
+
+  setCurrentAuth() {
+    const authString = localStorage.getItem('auth');
+    if (!authString) return;
+    const auth: Auth = JSON.parse(authString);
+    this.authService.setCurrentAuth(auth);
+  }
 }
