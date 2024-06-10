eb337e8a772d4a0d8a8382c1fb5e82e58babb5f5
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-06-09 22:26:05 -0400

    feat: create auth guard

---
+++ src/app/_guards/auth.guard.ts
@@ -0,0 +1,14 @@
+import { inject } from '@angular/core';
+import { CanActivateFn } from '@angular/router';
+import { AuthService } from '../_services/auth.service';
+import { map } from 'rxjs';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+
+  return authService.currentAuth$.pipe(
+    map((auth) => {
+      return auth != null;
+    })
+  );
+};
